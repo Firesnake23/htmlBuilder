898865740eb0ebd0611139e91d5195387dbdd884
Firesnake23 <alexluethi@belponline.ch>
2017-01-15 18:39:03 +0100

    Update
    every possible tag can be created now.
    The tags br,hr and img are not usable yet

---
+++ body/h6.class.php
@@ -0,0 +1,50 @@
+<?php
+	require_once $rootFolder.'htmlBuilder/body/htmlElement.class.php';
+	class H6 extends htmlElement{
+		public function __construct($parent){
+			$this->level = $parent->getLevel() + 1;
+    		$this->name = 'h6';
+    		$this->parent = $parent;
+		}
+
+		public function setId($id){
+			$this->id = $id;
+		}
+
+		public function setContent($content){
+			$this->content = $content;
+		}
+
+		public function setName($name){
+			$this->elementName = $name;
+		}
+
+		public function setClass($class){
+			$this->class = $class;
+		}
+
+		public function addStyle(Style $style){
+			$len = count($this->styles);
+    		$this->styles[$len] = $style;		
+		}
+
+		public function addJsEvent(JavaScriptEvent $jsEvent){
+			$len = count($this->jsEvents);
+    		$this->jsEvents[$len] = $jsEvent;		
+		}
+
+		public function addChild(HtmlElement $child){
+			$len = count($this->children);
+    		$this->children[$len] = $child;		
+		}
+
+		public function addAttribute(CustomAttribute $attribute){
+			$len = count($this->customAttributes);
+    		$this->customAttributes[$len] = $attribute;		
+		}
+
+		public function hide(){
+			$this->hidden = true;
+		}
+	}
+?>
---
+++ body/td.class.php
@@ -0,0 +1,50 @@
+<?php
+	require_once $rootFolder.'htmlBuilder/body/htmlElement.class.php';
+	class Td extends htmlElement{
+		public function __construct($parent){
+			$this->level = $parent->getLevel() + 1;
+    		$this->name = 'td';
+    		$this->parent = $parent;
+		}
+
+		public function setId($id){
+			$this->id = $id;
+		}
+
+		public function setContent($content){
+			$this->content = $content;
+		}
+
+		public function setName($name){
+			$this->elementName = $name;
+		}
+
+		public function setClass($class){
+			$this->class = $class;
+		}
+
+		public function addStyle(Style $style){
+			$len = count($this->styles);
+    		$this->styles[$len] = $style;		
+		}
+
+		public function addJsEvent(JavaScriptEvent $jsEvent){
+			$len = count($this->jsEvents);
+    		$this->jsEvents[$len] = $jsEvent;		
+		}
+
+		public function addChild(HtmlElement $child){
+			$len = count($this->children);
+    		$this->children[$len] = $child;		
+		}
+
+		public function addAttribute(CustomAttribute $attribute){
+			$len = count($this->customAttributes);
+    		$this->customAttributes[$len] = $attribute;		
+		}
+
+		public function hide(){
+			$this->hidden = true;
+		}
+	}
+?>
